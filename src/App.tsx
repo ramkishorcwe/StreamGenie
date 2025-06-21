ce98e61f805cea6b681203987a37af27803049e7
RamKishor <ramkishor142000@gmail.com>
2025-06-21 23:08:00 +0530

    firebase setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+//router and protected router
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <>Home</>
+    },
+    {
+      path: "/login",
+      element: <>login</>
+    },
+    {
+      path: "/",
+      element: <>Home</>
+    },
+
+  ])
   return (
+
     <div className='text-zinc-600'>
+      <RouterProvider router={router} />
       Tailwind Configured
     </div>
   )

356d4dbd0c261687ac3e3c40fff107181b7a1586
RamKishor <ramkishor142000@gmail.com>
2025-07-19 16:05:36 +0530

    add autoplay video on home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,7 +4,7 @@ import type { IStore } from "../store/store";
 // import fetchMovie from "./custom-hook/fetch-movie";
 import MoviesList from "./movie-list";
 import type { IMovie, IMovies } from "../store/movies";
-// import HeroContainer from "./hero-video";
+import HeroContainer from "./hero-video";
 import { useEffect } from "react";
 import { setMovies } from "../store/movies";
 import resource from "../resource";
@@ -17,11 +17,11 @@ export interface IAllMovie {
 
 const Home = () => {
   const dispatch = useDispatch();
-  const movies:any = useSelector((store: IStore) => store.movies);
+  const movies: any = useSelector((store: IStore) => store.movies);
 
   useEffect(() => {
-    resource.endPoints.forEach((endPoint:string)=>{
-    fetchMovies(endPoint)
+    resource.endPoints.forEach((endPoint: string) => {
+      fetchMovies(endPoint)
     })
   }, []);
   const fetchMovies = async (uri: string) => {
@@ -36,10 +36,10 @@ const Home = () => {
       });
       const movies: any = await response.json();
       const payloadToSetStore = {
-            page: movies.page,
-            results: [...movies.results],
-            total_pages: movies["total_pages"],
-            total_results: movies["total_results"]
+        page: movies.page,
+        results: [...movies.results],
+        total_pages: movies["total_pages"],
+        total_results: movies["total_results"]
       }
       const catagreeNameKey: string[] = uri.split("/");
       dispatch(
@@ -56,7 +56,7 @@ const Home = () => {
     // <div className="bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/7d2359a4-434f-4efa-9ff3-e9d38a8bde7f/web/IN-en-20250707-TRIFECTA-perspective_4faa9280-a2c5-4e07-aafc-a45ce43fea09_large.jpg')] opacity-100">
     <div className="bg-[url('https://plus.unsplash.com/premium_photo-1668473366952-45f06fbf6492')] opacity-100">
       <Header />
-      {/* <HeroContainer /> */}
+      <HeroContainer />
       <div className="w-[97%]">
         {Object.keys(movies).map((listType: string) => (
           <MoviesList
